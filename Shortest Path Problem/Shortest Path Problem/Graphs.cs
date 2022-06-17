8b9b7d10001e8a28da1364ff80a1fd49158937eb
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2022-06-17 19:55:12 +1000

    Graph Classes
    Need to work on A* Pathfinding, but Djinkstras works for the time being.
    
    Happy with the OOP :D

--- Shortest Path Problem/Shortest Path Problem/Graphs.cs
+++ Shortest Path Problem/Shortest Path Problem/Graphs.cs
@@ -6,40 +6,80 @@ using System.Threading.Tasks;
 
 namespace Shortest_Path_Problem
 {
-    public class Graphs
-    {
-        #region Neighbourhood of a Vertex
-        /// <summary>
-        /// Return the neighbourhood of a vertex u in graph (V, E)
-        /// </summary>
-        /// <param name="V"></param>
-        /// <param name="E"></param>
-        /// <param name="u"></param>
-        /// <returns></returns>
-        HashSet<object> N(HashSet<object> V, HashSet<(object, object)> E, object u) {
+    public class Graph {
+
+        public HashSet<object> Vertices { get; private set; }
+        public HashSet<(object, object)> Edges { get; private set; }
+
+        #region Constructors
+        public Graph() { }
+
+        public Graph(HashSet<object> Vertices = null, HashSet<(object, object)> Edges = null) {
+            this.Vertices = Vertices;
+            this.Edges = Edges;
+        }
+        #endregion
+
+        #region Conversions
+        public WeightedGraph ToWeightedGraph() {
+            WeightedGraph weightedGraph = new(this);
+            weightedGraph.SetGraph(this);
+            return weightedGraph;
+        }
+        #endregion
+
+        #region Vertex and Edge Management
+        public virtual void SetGraph(Graph graph) {
+            Vertices = graph.Vertices;
+            Edges = graph.Edges;
+        }
+
+        public virtual void AddVertex(object vertex) {
+            Vertices.Add(vertex);
+        }
+
+        public virtual void RemoveVertex(object vertex) {
+            Vertices.Remove(vertex);
+            foreach ((object, object) edge in Edges) {
+                if (edge.Item1 == vertex) { Edges.Remove(edge); continue; }
+                if (edge.Item2 == vertex) { Edges.Remove(edge); continue; }
+                //Catch Error Here
+            }
+        }
+
+        public virtual void AddEdge((object, object) edge) {
+            if (!Vertices.Contains(edge.Item1) && !Vertices.Contains(edge.Item2)) { /*Catch Error*/ return; }
+            Edges.Add(edge);
+        }
+
+        public virtual void RemoveEdge((object, object) edge) {
+            Edges.Remove(edge);
+        }
+        #endregion
+
+        #region Neighbourhoods
+        public HashSet<object> N(HashSet<object> V, object u)
+        {
             var neighbourhood = new HashSet<object>();
-            foreach (object v in V) {
-                if (E.Contains((u, v))){
+            foreach (object v in V)
+            {
+                if (Edges.Contains((u, v)))
+                {
                     neighbourhood.Add(v);
                 }
             }
             return neighbourhood;
         }
-        #endregion
 
-        #region Neighbourhood of a set of Vertices
-        /// <summary>
-        /// Return the neighbourhood of a set of vertices S in graph (V, E)
-        /// </summary>
-        /// <param name="V"></param>
-        /// <param name="E"></param>
-        /// <param name="S"></param>
-        /// <returns></returns>
-        HashSet<object> NS(HashSet<object> V, HashSet<(object, object)> E, HashSet<object> S) {
+        HashSet<object> NS(HashSet<object> S)
+        {
             var neighbourhood = new HashSet<object>();
-            foreach (object v in V) {
-                foreach (object u in S) {
-                    if (E.Contains((u, v))) {
+            foreach (object v in Vertices)
+            {
+                foreach (object u in S)
+                {
+                    if (Edges.Contains((u, v)))
+                    {
                         neighbourhood.Add(v);
                     }
                 }
@@ -48,28 +88,48 @@ namespace Shortest_Path_Problem
         }
         #endregion
 
-        #region Distance Classes
-        List<HashSet<object>> DistanceClasses(HashSet<object> V, HashSet<(object, object)> E, object u) {
+        #region SSP
+        public List<object> SolveShortestPath(object root, object destination)
+        {
+            var parents = SpanTree(Vertices, Edges, root);
+            List<object> shortestPath = new();
+            PathFromTree(parents, destination, ref shortestPath);
+            return shortestPath;
+        }
+
+        void PathFromTree(Dictionary<object, object> parents, object destination, ref List<object> path)
+        {
+            var parent = parents[destination];
+            if (parent == null) return;
+            path.Add(parent);
+            PathFromTree(parents, parent, ref path);
+        }
+        #endregion
+
+        #region DistanceClasses
+        public List<HashSet<object>> GetDistanceClasses(object u) {
             HashSet<object> uSet = new HashSet<object>();
             uSet.Add(u);
             var D = new List<HashSet<object>>();
-            return DistanceClassesR(V, E, D);
+            return DistanceClassesR(Vertices, Edges, D);
         }
 
         List<HashSet<object>> DistanceClassesR(HashSet<object> V, HashSet<(object, object)> E, List<HashSet<object>> D) {
             var lastClass = D[D.Count];
             var Vnew = new HashSet<object>();
-            foreach (var item in lastClass) {
+            foreach (var item in lastClass)
+            {
                 Vnew.Remove(item);
             }
             if (Vnew.Count == 0) return D;
-            D.Add(NS(V, E, lastClass));
-            return DistanceClassesR(V, E, D);
+            D.Add(NS(lastClass));
+            return DistanceClassesR(Vertices, Edges, D);
         }
         #endregion
 
         #region Span Tree
-        public Dictionary<object, object> SpanTree(HashSet<object> V, HashSet<(object, object)> E, object r) {
+        Dictionary<object, object> SpanTree(HashSet<object> V, HashSet<(object, object)> E, object r)
+        {
             var parents = new Dictionary<object, object>();
             V.Remove(r);
             var D = new HashSet<object>();
@@ -79,47 +139,101 @@ namespace Shortest_Path_Problem
             return parents;
         }
 
-        void SpanTreeR(ref HashSet<object> V, ref HashSet<(object, object)> E, ref HashSet<object> D, ref Dictionary<object, object> parents) {
-            var Dnew = NS(V, E, D);
+        void SpanTreeR(ref HashSet<object> V, ref HashSet<(object, object)> E, ref HashSet<object> D, ref Dictionary<object, object> parents)
+        {
+            var Dnew = NS(D);
             if (Dnew.Count == 0) return;
-            foreach (object v in Dnew) {
-                parents[v] = N(D, E, v).ToArray()[0];
+            foreach (object v in Dnew)
+            {
+                parents[v] = N(Vertices, v).ToArray()[0];
             }
-            foreach (object v in Dnew) {
+            foreach (object v in Dnew)
+            {
                 V.Remove(v);
             }
             SpanTreeR(ref V, ref E, ref Dnew, ref parents);
         }
+        #endregion
 
-        public void PathFromTree(Dictionary<object, object> parents, object destination, ref List<object> path) {
-            var parent = parents[destination];
-            if (parent == null) return;
-            path.Add(parent);
-            PathFromTree(parents, parent, ref path);
+    }
+
+    public class WeightedGraph : Graph {
+
+        public Dictionary<(object, object), float> Weights { get; private set; }
+
+        #region Constructors
+        public WeightedGraph() { }
+
+        public WeightedGraph(Graph graph = null) {
+            SetGraph(graph);
+        }
+
+        public WeightedGraph(HashSet<object> Vertices = null, HashSet<(object, object)> Edges = null, Dictionary<(object, object), float> Weights = null) {
+            SetGraph(new Graph(Vertices, Edges));
+            this.Weights = Weights;
         }
         #endregion
 
-        #region Dijkstra's Algorithm
-        
+        #region Overrides
+        public override void SetGraph(Graph graph) {
+            base.SetGraph(graph);
+            foreach ((object, object) edge in Edges)
+            {
+                Weights[edge] = int.MaxValue;
+            }
+        }
+
+        public override void AddEdge((object, object) edge) {
+            base.AddEdge(edge);
+            Weights[edge] = int.MaxValue;
+        }
+
+        public override void RemoveEdge((object, object) edge) {
+            base.RemoveEdge(edge);
+            Weights.Remove(edge);
+        }
+
+        #endregion
+
+        #region Conversions
+        public Graph ToGraph() {
+            Graph graph = new();
+            graph.SetGraph(this);
+            return graph;
+        }
+        #endregion
+
+        #region Weight Management
+        public float GetWeight((object, object) edge) {
+            return Weights[edge];
+        }
+
+        public void UpdateWeight((object, object) edge, float value) {
+            Weights[edge] = value;
+        }
+        #endregion
+
+        #region Weighted Pathfinding
         /// <summary>
-        /// Given a graph (V, E) with a weight function, returns functions 'parents' and 'distance' for future pathfinding.
+        /// Djikstra's Terminating once the whole graph has been processed.
         /// </summary>
-        /// <param name="V">A Set of Vertices.</param>
-        /// <param name="E">A Set of Edges.</param>
-        /// <param name="weights">A Weight Function of 'E'.</param>
-        /// <param name="root">The root vertex.</param>
-        /// <param name="parents">Relates vertex 'u' to 'v' such that the relation is the shortest path to the root.</param>
-        /// <param name="distance">Relates vertex 'u' to a total weighted distance from the root.</param>
-        public void ShortestWeightedPath(HashSet<object> V, HashSet<(object, object)> E, ref Dictionary<(object, object), int> weights, object root, out Dictionary<object, object> parents, out Dictionary<object, int> distance) {
+        /// <param name="weights"></param>
+        /// <param name="root"></param>
+        /// <param name="parents"></param>
+        /// <param name="distance"></param>
+        public void SpanShortestWeightedPath(ref Dictionary<(object, object), float> weights, object root, out Dictionary<object, object> parents, out Dictionary<object, float> distance)
+        {
             //Setup
-            HashSet<object> S = new ();
+            HashSet<object> S = new();
             distance = new();
             List<object> Q = new();
             parents = new();
-            foreach (object v in V) {
+            foreach (object v in Vertices)
+            {
                 Q.Add(v);
             }
-            foreach (object v in V) {
+            foreach (object v in Vertices)
+            {
                 distance[v] = int.MaxValue;
             }
             parents[root] = null;
@@ -128,16 +242,18 @@ namespace Shortest_Path_Problem
             object w;
 
             //Dijkstras Algorithm
-            while (true) {
+            while (true)
+            {
                 // 'w' being the next vertice to check.
                 // Currently set to the next shortest path in the priority queue 'Q'
-                // Up for optimisation, but works for now
                 w = Q[0];
-                var vN = validNeighbours(V, E, S, w);
+                var vN = validNeighbours(Vertices, S, w);
                 // Foreach neighbour of 'w' that hasn't had it's shortest path solved.
-                foreach (object v in vN) {
-                    var dist = distance[w] + GetWeightOfEdge((v, w), ref weights);
-                    if (distance[v] >= dist) {
+                foreach (object v in vN)
+                {
+                    var dist = distance[w] + GetWeightOfEdge((v, w), weights);
+                    if (distance[v] >= dist)
+                    {
                         distance[v] = dist;
                         parents[v] = w;
                     }
@@ -149,30 +265,102 @@ namespace Shortest_Path_Problem
                 if (Q.Count == 0) break;
             }
         }
+        
+        /// <summary>
+        /// Djikstra's Terminating once the destination has been processed.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="destination"></param>
+        /// <param name="parents"></param>
+        /// <param name="distance"></param>
+        public void ShortestWeightedPathToDestination(object root, object destination, out Dictionary<object, object> parents, out Dictionary<object, float> distance)
+        {
+            //Setup
+            HashSet<object> S = new();
+            distance = new();
+            List<object> Q = new();
+            parents = new();
+            foreach (object v in Vertices)
+            {
+                Q.Add(v);
+            }
+            foreach (object v in Vertices)
+            {
+                distance[v] = int.MaxValue;
+            }
+            parents[root] = null;
+            distance[root] = 0;
+            SortQueueByWeight(ref Q, distance);
+            object w;
 
-        public void SortQueueByWeight(ref List<object> Q, Dictionary<object, int> t){
+            //Dijkstras Algorithm
+            while (true)
+            {
+                // 'w' being the next vertice to check.
+                // Currently set to the next shortest path in the priority queue 'Q'
+                w = Q[0];
+                var vN = validNeighbours(Vertices, S, w);
+                // Foreach neighbour of 'w' that hasn't had it's shortest path solved.
+                foreach (object v in vN)
+                {
+                    var dist = distance[w] + GetWeightOfEdge((v, w), Weights);
+                    if (distance[v] >= dist)
+                    {
+                        distance[v] = dist;
+                        parents[v] = w;
+                    }
+                }
+                // Reorganise Priority Queue by the next node with the shortest current distance.
+                SortQueueByWeight(ref Q, distance);
+                S.Add(w);
+                Q.Remove(w);
+                if (w == destination) break;
+            }
+        }
+        #endregion
+
+        #region Supporting Functions
+        public void SortQueueByWeight(ref List<object> Q, Dictionary<object, float> t)
+        {
             Q = Q.OrderBy(x => t[x]).ToList();
         }
 
-        public void RemoveSolvedVertsFromQueue(ref List<object> Q, HashSet<object> S) {
-            for (int i = 0; i < Q.Count; i++) {
-                if (S.Contains(Q[i])) {
+        public void RemoveSolvedVertsFromQueue(ref List<object> Q, HashSet<object> S)
+        {
+            for (int i = 0; i < Q.Count; i++)
+            {
+                if (S.Contains(Q[i]))
+                {
                     Q.Remove(Q[i]);
                 }
             }
         }
-        HashSet<object> validNeighbours(HashSet<object> V, HashSet<(object, object)> E, HashSet<object> S, object u) {
+
+        HashSet<object> validNeighbours(HashSet<object> V, HashSet<object> S, object u)
+        {
             HashSet<object> neighbours = new();
-            foreach (object neighbour in N(V, E, u)) {
+            foreach (object neighbour in N(V, u))
+            {
                 if (!S.Contains(neighbour)) neighbours.Add(neighbour);
             }
             return neighbours;
         }
 
-        int GetWeightOfEdge((object, object) edge, ref Dictionary<(object, object), int> weights) {
+        float GetWeightOfEdge((object, object) edge, Dictionary<(object, object), float> weights)
+        {
             if (weights.ContainsKey(edge)) return weights[edge];
             return weights[(edge.Item2, edge.Item1)];
         }
         #endregion
     }
+
+    public class HeuristicGraph : WeightedGraph {
+        /// <summary>
+        /// I think something important to note which was very only briefly suggested is that if your distance-to-goal heuristic always underestimates you will always find the shortest path, but if not then the path you get may not be the shortest (which for some problems may be suitable).
+        /// If you underestimate too much then the benefits of A* diminish and you'll explore more and more of the graph. Additionally, Dijkstra is a generalisation of A* where the distance-to-goal is always underestimated as 0.
+        /// </summary>
+        public Dictionary<object, float> a;
+
+    }
+    
 }
--- Shortest Path Problem/Shortest Path Problem/Program.cs
+++ Shortest Path Problem/Shortest Path Problem/Program.cs
@@ -8,12 +8,15 @@ namespace Shortest_Path_Problem
     {
         static void Main(string[] args)
         {
-            Graphs g = new Graphs();
+            WeightedGraph g = new(GetVertices(), GetEdges(), GetWeightFunction());
+
             var vertices = new HashSet<object>(GetVertices());
             var edges = new HashSet<(object, object)>(GetEdges());
             var weights = GetWeightFunction();
             Dictionary<object, object> parents;
             Dictionary<object, int> distance;
+            g.
+            g.ShortestWeightedPathToDestination();
             g.ShortestWeightedPath(vertices, edges, ref weights, 'A', out parents, out distance);
             List<object> path = new();
             g.PathFromTree(parents, 'E', ref path);
@@ -22,24 +25,27 @@ namespace Shortest_Path_Problem
             }
         }
 
-        static object[] GetVertices() {
-            object[] output = { 'A', 'B', 'C', 'D', 'E', 'F' };
-            return output;
-        }
-
-        static (object, object)[] GetEdges() {
-            (object, object)[] uniqueEdges = { ('A', 'D'), ('D', 'C'), ('C', 'E'), ('C', 'F'), ('A', 'B'), ('B', 'C'), ('B', 'F') };
-            var len = uniqueEdges.Length;
-            var output = new (object, object)[2 * len];
-            for (int i = 0; i < len; i++) {
-                output[i] = uniqueEdges[i];
-                output[i + len] = (uniqueEdges[i].Item2, uniqueEdges[i].Item1);
+        static HashSet<object> GetVertices() {
+            object[] verts = { 'A', 'B', 'C', 'D', 'E', 'F' };
+            HashSet<object> output = new();
+            foreach (object vert in verts) {
+                output.Add(vert);
             }
             return output;
         }
 
-        static Dictionary<(object, object), int> GetWeightFunction() {
-            Dictionary<(object, object), int> weights = new Dictionary<(object, object), int>();
+        static HashSet<(object, object)> GetEdges() {
+            (object, object)[] uniqueEdges = { ('A', 'D'), ('D', 'C'), ('C', 'E'), ('C', 'F'), ('A', 'B'), ('B', 'C'), ('B', 'F') };
+            HashSet<(object, object)> output = new();
+            foreach ((object, object) edge in uniqueEdges) {
+                output.Add((edge.Item1, edge.Item2));
+                output.Add((edge.Item2, edge.Item1));
+            }
+            return output;
+        }
+
+        static Dictionary<(object, object), float> GetWeightFunction() {
+            Dictionary<(object, object), float> weights = new Dictionary<(object, object), float>();
             weights.Add(('A', 'D'), 2);
             weights.Add(('A', 'B'), 1);
             weights.Add(('D', 'C'), 2);
